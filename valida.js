ff98bcc405f928736d6d81db87879b3627815703
mateusrmelo-collab <mateus.r.melo@edu.senai.br>
2026-03-29 02:11:39 -0300

    addjs

---
+++ valida.js
@@ -0,0 +1,20 @@
+function validarSenha() {
+  const senhaCorreta = "1234"; 
+  const senhaDigitada = document.getElementById("senha").value;
+  const mensagemErro = document.getElementById("mensagem-erro");
+
+  if (senhaDigitada === senhaCorreta) {
+    window.location.href = "index.html"; 
+    return false;
+  } else {
+    const audio = new Audio("../audio/0329.mp4"); 
+    audio.play();
+
+    mensagemErro.style.display = "block";
+
+    document.getElementById("senha").value = "";
+
+    return false;
+  }
+}
+
